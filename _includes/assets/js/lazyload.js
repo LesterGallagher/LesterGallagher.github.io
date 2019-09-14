4f1295329467d88a654f5ce06155395eaecf3ba1
Sem Postma <sem.postma@gmail.com>
2019-09-14 20:05:15 +0200

    layout fixes and additions.

--- _includes/assets/js/lazyload.js
+++ _includes/assets/js/lazyload.js
@@ -1,37 +1,44 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  var lazyImages = [].slice.call(document.getElementsByClassName('lazyload'));
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = [].slice.call(document.getElementsByClassName("lazyload"));
   var active = false;
 
   function lazyLoad() {
     if (active === false) {
       active = true;
-      setTimeout(function () {
+      setTimeout(function() {
         for (var i = lazyImages.length - 1; i >= 0; i--) {
-          if (lazyImages[i].classList.contains('loaded') === false
-            && lazyImages[i].getBoundingClientRect().top <= window.innerHeight + 600 
-            && lazyImages[i].getBoundingClientRect().bottom >= -200) {
-            (function () {
+          if (
+            lazyImages[i].classList.contains("loaded") === false &&
+            lazyImages[i].getBoundingClientRect().top <=
+              window.innerHeight + 600 &&
+            lazyImages[i].getBoundingClientRect().bottom >= -200
+          ) {
+            (function() {
               var lazyImage = lazyImages[i];
               var img = new Image();
-              img.onload = img.onerror = function () {
+
+              img.onload = img.onerror = function() {
                 lazyImage.src = img.src;
-                lazyImage.classList.add('loaded');
+                lazyImage.classList.add("loaded");
               };
-              img.src = lazyImage.getAttribute('data-src');
+
+              img.src = lazyImage.getAttribute("data-src");
             })();
+
             lazyImages.splice(i, 1);
           }
         }
+
         if (lazyImages.length === 0) {
           document.removeEventListener("scroll", lazyLoad);
           window.removeEventListener("resize", lazyLoad);
           window.removeEventListener("orientationchange", lazyLoad);
         }
+
         active = false;
       }, 200);
     }
-  };
+  }
 
   document.addEventListener("scroll", lazyLoad);
   window.addEventListener("resize", lazyLoad);
